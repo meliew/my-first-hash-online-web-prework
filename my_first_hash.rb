2a5d177386b471b7580686b41ef959007de41091
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-04 21:12:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def retrieval
   }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifst["oil paintings"]
+  shipping_manifest[2]
 end
 
 def adding
@@ -43,6 +43,6 @@ def adding
   shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-  shipping_manifest 
+
 
 end
