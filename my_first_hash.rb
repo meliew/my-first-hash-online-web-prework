7c88c6ca77fe0caf9c41319887c4c42f441bc978
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-04 21:07:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
-def my_hash  
+def my_hash
   your_hash = {"cat" => "Cappy", "dog" => "Soldier", "frog" => "Sydney"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+
 end
 
 
